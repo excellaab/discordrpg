ffedb431279ab44aa224f010180c9cdaca590c39
excellaab <axelbintoro433@gmail.com>
2026-08-26 03:23:43 +0700

    refactor: db exception handler decorator

--- core/lib/db.py
+++ core/lib/db.py
@@ -3,83 +3,60 @@ import json
 import inspect
 from functools import wraps
 from core.lib.log import dblogger
+import discord
 dbpool: asyncpg.Pool = None
 
-async def new_player(user, class_name):
-    try:
-        async with dbpool.acquire() as conn:
-            response = await conn.fetchrow('''
-                INSERT INTO players (user_id, class) 
-                VALUES ($1, $2) 
-                ON CONFLICT (user_id) DO NOTHING
-                RETURNING user_id;
-            ''', user.id, class_name)
-            
-            return response, False
-    except asyncpg.UndefinedTableError as e:
-        dblogger.exception(f"Missing DB table while registering user {user.id}: {e}")
-        return None, True
-    except asyncpg.UndefinedColumnError as e:
-        dblogger.exception(f"Missing DB column while registering user {user.id}: {e}")
-        return None, True
-    except Exception as e:
-        dblogger.exception(f"DB error registering user {user.id}: {e}")
-        return None, True
+def dbExceptionHandler(func):
+    @wraps(func)
+    async def wrapper(user: discord.User, *args, **kwargs):
+        try:
+            async with dbpool.acquire() as conn:
+                return await func(user, conn, *args, **kwargs)
+        except asyncpg.UndefinedTableError as e:
+            dblogger.exception(f"Missing DB table. UserID: {user.id}. Error: {e}")
+            return None, True
+        except asyncpg.UndefinedColumnError as e:
+            dblogger.exception(f"Missing DB column. UserID: {user.id}. Error: {e}")
+            return None, True
+        except Exception as e:
+            dblogger.exception(f"DB error occured. UserID: {user.id}. Error: {e}")
+            return None, True
+    return wrapper
 
-async def set_class(user, class_name):
-    try:
-        async with dbpool.acquire() as conn:
-            await conn.execute('''
-                UPDATE players 
-                SET class = $1 
-                WHERE user_id = $2
-            ''', class_name, user.id)
-    except asyncpg.UndefinedTableError as e:
-            dblogger.exception(f"Missing DB table while setting class for user {user.id}: {e}")
-            return True
-    except asyncpg.UndefinedColumnError as e:
-        dblogger.exception(f"Missing DB column while setting class for user {user.id}: {e}")
-        return True
-    except Exception as e:
-        dblogger.exception(f"DB error setting class for user {user.id}: {e}")
-        return True
-    return False
+@dbExceptionHandler
+async def new_player(user: discord.User, conn: asyncpg.Connection, class_name: str):
+    response = await conn.fetchrow('''
+        INSERT INTO players (user_id, class) 
+        VALUES ($1, $2) 
+        ON CONFLICT (user_id) DO NOTHING
+        RETURNING user_id;
+    ''', user.id, class_name)
+    
+    return response, False
 
-async def fetch_player(user):
-    try:
-        async with dbpool.acquire() as conn:
-            player = await conn.fetchrow('SELECT * FROM players WHERE user_id = $1', user.id)
-            return player, False
-    except asyncpg.UndefinedTableError as e:
-        dblogger.exception(f"Missing DB table while fetching profile for user {user.id}: {e}")
-        return None, True
-    except asyncpg.UndefinedColumnError as e:
-        dblogger.exception(f"Missing DB column while fetching profile for user {user.id}: {e}")
-        return None, True
-    except Exception as e:
-        dblogger.exception(f"DB error fetching profile for user {user.id}: {e}")
-        return None, True
+@dbExceptionHandler
+async def set_class(user: discord.User, conn: asyncpg.Connection, class_name: str):
+    await conn.execute('''
+        UPDATE players 
+        SET class = $1 
+        WHERE user_id = $2
+    ''', class_name, user.id)
 
-async def fetch_equipped_armor(user):
-    try:
-        async with dbpool.acquire() as conn:
-            armor = await conn.fetchrow('''
-                SELECT i.instance_state 
-                FROM player_equipment e
-                JOIN player_inventory i ON e.armor_instance_id = i.instance_id
-                WHERE e.user_id = $1
-            ''', user.id)
+@dbExceptionHandler
+async def fetch_player(user: discord.User, conn: asyncpg.Connection):
+    player = await conn.fetchrow('SELECT * FROM players WHERE user_id = $1', user.id)
+    return player, False
 
-            return armor, False
-    except asyncpg.UndefinedTableError as e:
-        dblogger.exception(f"Missing DB table while fetching armor for user {user.id}: {e}")
-        return None, True
-    except asyncpg.UndefinedColumnError as e:
-        dblogger.exception(f"Missing DB column while fetching armor for user {user.id}: {e}")
-        return None, True
-    except Exception as e:
-        dblogger.exception(f"DB error fetching armor for user {user.id}: {e}")
-        return None, True
+@dbExceptionHandler
+async def fetch_equipped_armor(user: discord.User, conn: asyncpg.Connection):
+    armor = await conn.fetchrow('''
+        SELECT i.instance_state 
+        FROM player_equipment e
+        JOIN player_inventory i ON e.armor_instance_id = i.instance_id
+        WHERE e.user_id = $1
+    ''', user.id)
+
+    return armor, False
 
 def with_player_context(func):
     @wraps(func)
@@ -119,56 +96,31 @@ def with_player_context(func):
         
     return wrapper
 
-async def levelup(user):
-    try:
-        async with dbpool.acquire() as conn:
-            pass
-    except asyncpg.UndefinedTableError as e:
-                dblogger.exception(f"Missing DB table while executing level up event for user {user.id}: {e}")
-                return True, None
-    except asyncpg.UndefinedColumnError as e:
-        dblogger.exception(f"Missing DB column while executing level up event for user {user.id}: {e}")
-        return True, None
-    except Exception as e:
-        dblogger.exception(f"DB error while executing level up event for user {user.id}: {e}")
-        return True, None
-    return False, None
+@dbExceptionHandler
+async def levelup(user: discord.User, conn: asyncpg.Connection):
+    pass
 
-async def startrun(user, hp):
-    try:
-        async with dbpool.acquire() as conn:
-            response = await conn.fetchrow('''
-                INSERT INTO runs (user_id, hp, rank_snapshot)
-                SELECT $1, $2, rank
-                FROM players
-                WHERE user_id = $1
-                ON CONFLICT (user_id) DO NOTHING
-                RETURNING *;
-            ''', user.id, hp)
-            
-            return response, False
-    except asyncpg.UndefinedTableError as e:
-        dblogger.exception(f"Missing DB table while starting run for {user.id}: {e}")
-        return None, True
-    except asyncpg.UndefinedColumnError as e:
-        dblogger.exception(f"Missing DB column while starting run for {user.id}: {e}")
-        return None, True
-    except Exception as e:
-        dblogger.exception(f"DB error starting run for {user.id}: {e}")
-        return None, True
+@dbExceptionHandler
+async def startrun(user: discord.User, conn: asyncpg.Connection, hp: int):
+    response = await conn.fetchrow('''
+        INSERT INTO runs (user_id, hp, rank_snapshot)
+        SELECT $1, $2, rank
+        FROM players
+        WHERE user_id = $1
+        ON CONFLICT (user_id) DO NOTHING
+        RETURNING *;
+    ''', user.id, hp)
+    
+    return response, False
 
-async def fetchrun(user):
-    try:
-        async with dbpool.acquire() as conn:
-            run = await conn.fetchrow('SELECT * FROM runs WHERE user_id = $1', user.id)
-            
-            return run, False
-    except asyncpg.UndefinedTableError as e:
-        dblogger.exception(f"Missing DB table while fetching run for {user.id}: {e}")
-        return None, True
-    except asyncpg.UndefinedColumnError as e:
-        dblogger.exception(f"Missing DB column while fetching run for {user.id}: {e}")
-        return None, True
-    except Exception as e:
-        dblogger.exception(f"DB error fetching run for {user.id}: {e}")
-        return None, True
+@dbExceptionHandler
+async def fetchrun(user: discord.User, conn: asyncpg.Connection):
+    run = await conn.fetchrow('SELECT * FROM runs WHERE user_id = $1', user.id)
+    
+    return run, False
+
+@dbExceptionHandler
+async def endrun(user: discord.User, conn: asyncpg.Connection):
+    await conn.fetchrow('DELETE FROM runs WHERE user_id = $1', user.id)
+    
+    return False
